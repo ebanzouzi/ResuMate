7c3cee0aa06b3d0fda8ad0b5eac208b226d3a8cd
ebanzouzi <ebanzouzi0@gmail.com>
2026-06-14 15:10:28 +0100

    Ajout de Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { Buffer } from 'buffer';
+import { Buffer } from "buffer";
 window.Buffer = Buffer; // Rend l'objet disponible pour @react-pdf/renderer
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <SpeedInsights />
     <App />
   </StrictMode>,
-)
+);
