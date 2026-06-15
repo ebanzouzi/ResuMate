3940afb4532d779c9e3c294fbdfd7ac59be134d0
ebanzouzi <ebanzouzi0@gmail.com>
2026-06-15 12:04:27 +0100

    Ajout complet de Vercel Analytics et Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ window.Buffer = Buffer; // Rend l'objet disponible pour @react-pdf/renderer
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Analytics />
     <SpeedInsights />
     <App />
   </StrictMode>,
